c247c1bca713d19e513fe38c4d4a42925e728e2f
Indra Arianggi Suryaatmaja <indraarianggi@gmail.com>
2021-08-19 11:48:22 +0700

    [course-goals] styled components

--- 02-course-goals/src/components/CourseGoals/CourseInput/CourseInput.tsx
+++ 02-course-goals/src/components/CourseGoals/CourseInput/CourseInput.tsx
@@ -1,6 +1,40 @@
 import React, { useState } from "react";
+import styled from "styled-components";
 import Button from "../../UI/Button/Button";
-import "./CourseInput.css";
+// import "./CourseInput.css";
+
+interface IFormControlProps {
+  readonly invalid: boolean;
+}
+
+const FormControl = styled.div<IFormControlProps>`
+  & {
+    margin: 0.5rem 0;
+  }
+
+  & label {
+    color: ${(props) => (props.invalid ? "red" : "black")};
+    font-weight: bold;
+    display: block;
+    margin-bottom: 0.5rem;
+  }
+
+  & input {
+    display: block;
+    width: 100%;
+    border: 1px solid ${(props) => (props.invalid ? "red" : "#ccc")};
+    background: ${(props) => (props.invalid ? "salmon" : "transparent")};
+    font: inherit;
+    line-height: 1.5rem;
+    padding: 0 0.25rem;
+  }
+
+  & input:focus {
+    outline: none;
+    background: #fad0ec;
+    border-color: #8b005d;
+  }
+`;
 
 type TCourseInputProps = {
   onAddGoal: (input: string) => void;
@@ -8,22 +42,31 @@ type TCourseInputProps = {
 
 const CourseInput = ({ onAddGoal }: TCourseInputProps) => {
   const [enteredValue, setEnteredValue] = useState<string>("");
+  const [isValid, setIsValid] = useState<boolean>(true);
 
   const changeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (enteredValue.trim().length > 0) setIsValid(true);
+
     setEnteredValue(e.target.value);
   };
 
   const submitHandler = (e: React.SyntheticEvent) => {
     e.preventDefault();
+
+    if (enteredValue.trim().length === 0) {
+      setIsValid(false);
+      return;
+    }
+
     onAddGoal(enteredValue);
   };
 
   return (
     <form onSubmit={submitHandler}>
-      <div className="form-control">
+      <FormControl invalid={!isValid}>
         <label htmlFor="goal">Course Goal</label>
         <input type="text" name="goal" id="goal" onChange={changeHandler} />
-      </div>
+      </FormControl>
       <Button type="submit">Add Goal</Button>
     </form>
   );
--- 02-course-goals/src/components/UI/Button/Button.tsx
+++ 02-course-goals/src/components/UI/Button/Button.tsx
@@ -1,15 +1,45 @@
-type TButtonProps = {
-  type?: "button" | "submit" | "reset";
-  children: React.ReactNode;
-  onClick?: () => void;
-};
+import styled from "styled-components";
+// import "./Button.css";
 
-const Button = ({ type, children, onClick }: TButtonProps) => {
-  return (
-    <button type={type} className="button" onClick={onClick}>
-      {children}
-    </button>
-  );
-};
+const Button = styled.button`
+  width: 100%;
+  font: inherit;
+  padding: 0.5rem 1.5rem;
+  border: 1px solid #8b005d;
+  color: white;
+  background: #8b005d;
+  box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+  cursor: pointer;
+
+  /* Media Queries */
+  @media (min-width: 768px) {
+    width: auto;
+  }
+
+  &:focus {
+    outline: none;
+  }
+
+  &:hover,
+  &:active {
+    background: #ac0e77;
+    border-color: #ac0e77;
+    box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+  }
+`;
+
+// type TButtonProps = {
+//   type?: "button" | "submit" | "reset";
+//   children: React.ReactNode;
+//   onClick?: () => void;
+// };
+
+// const Button = ({ type, children, onClick }: TButtonProps) => {
+//   return (
+//     <button type={type} className="button" onClick={onClick}>
+//       {children}
+//     </button>
+//   );
+// };
 
 export default Button;
