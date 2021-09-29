cc324b5e2061013a99ac01c93820248e9c798480
Indra Arianggi Suryaatmaja <indraarianggi@gmail.com>
2021-09-29 10:28:57 +0700

    [nextjs-meetup-app] implement getStaticPaths and getStaticProps on the page with dynamic routes

--- 16-nextjs-meetup-app/pages/[meetupId]/index.tsx
+++ 16-nextjs-meetup-app/pages/[meetupId]/index.tsx
@@ -1,7 +1,42 @@
+import { GetStaticPaths, GetStaticProps, InferGetStaticPropsType } from "next";
 import React from "react";
 import MeetupDetails from "../../components/meetups/MeetupDetails";
 
-const MeetupDetailsPage = () => {
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [
+      {
+        params: { meetupId: "m1" },
+      },
+      {
+        params: { meetupId: "m2" },
+      },
+    ],
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const meetupId = context.params?.meetupId;
+  console.log(meetupId);
+
+  return {
+    props: {
+      meetupData: {
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
+        id: meetupId,
+        title: "A First Meetup",
+        address: "Some address 5, 12345 Some City",
+        description: "This is a first meetup!",
+      },
+    },
+  };
+};
+
+const MeetupDetailsPage = ({
+  meetupData,
+}: InferGetStaticPropsType<typeof getStaticProps>) => {
   return (
     <MeetupDetails
       image="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg"
